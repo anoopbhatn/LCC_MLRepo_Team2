71c0122335cdfee1ec85ec837a9bd067cf2bd8a7
amoghmahesh14 <40426374+amoghmahesh14@users.noreply.github.com>
2018-11-07 10:19:13 +0530

    Update p1.py
    Requested Changes has been made.

--- Task-2/p1.py
+++ Task-2/p1.py
@@ -1,8 +1,22 @@
-n1=int(input("Enter the start value\n"))
-n2=int(input("Enter the end value\n"))
-res=0
-for i in range(n1,n2+1):
+'''
+Created on Nov 1, 2018
+@author: amogh_mahesh
+
+Program to sum all the integer numbers in the give nrange
+'''
+#inputting start value
+start_value=int(input("Enter the start value\n"))
+
+#inputting end value
+end_value=int(input("Enter the end value\n"))
+
+#variable to store the final result
+result=0
+
+#a for loop to traverse through each number in the given range and add them
+for i in range(start_value,end_value+1):
 	res+=i
 
+#Printing out the result
 print("Result="+str(res))
 

a204dfd59beb04531b4c37766af8d55f453b2398
Ashritha27 <35839006+Ashritha27@users.noreply.github.com>
2018-11-08 00:12:04 +0530

    Create sum_range.cpp

---
+++ ashritha_shetty/sum_range.cpp
@@ -0,0 +1,20 @@
+/*
+Created on Novemberer 11, 2018
+@author: ashritha27
+*/
+//Program to find the sum of numbers in a given range in C++
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int min,max,sum=0;
+    cout<<"Enter the limits";
+    cin>>min;cin>>max;
+    for(int i=min;i<=max;i++){
+    sum+=i;
+    }
+    cout<<sum;
+    return 0;
+}

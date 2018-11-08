a0b22bdaad8bae3bb0f9b65eebfcbf7dde287d72
Prasada kp <44546156+prasadkpbhat@users.noreply.github.com>
2018-11-08 12:53:04 +0530

    Create sum.py
    Program to compute sum of all the integers in the given range

---
+++ prasada/sum.py
@@ -0,0 +1,17 @@
+'''
+Created on Thu Nov  8 12:34:32 2018
+@author: prasada
+input:two integers one is starting value and the ending value
+output:The sum of all the integers in the given range
+'''
+#initializing a variable to 0
+sum=0
+#assigning start value to a variable
+start=int(input("start value"))
+#assigning end value to a variable
+end=int(input("end value"))
+#for loop to calculate the sum of all the integers in the given range
+for i in range(start,end+1,1):
+    sum+=i;
+#printing the result
+print(sum)

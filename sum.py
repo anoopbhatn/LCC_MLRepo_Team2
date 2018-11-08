99f3eda8992d38dc852d02da2c9e04c588d7f4fa
apoorvakumar1998 <31769042+apoorvakumar1998@users.noreply.github.com>
2018-11-08 10:50:31 +0530

    Update and rename sum.py to sum_range.py

--- sum.py
+++
@@ -1,6 +0,0 @@
-sum=0;//variable to store sum
-start = int(input("Enter Start value:"))//inputing start value  
-end   = int(input("Enter End value:"))//inputing end value
- for i in range(start,end+1)://for loop to determine sum
-   sum=sum + i
-print(sum)  //printing sum value

c6ac0a299a2a71e4d4e735016ee3a73a76690636
Sumangala Rao <31768510+SumangalaRao@users.noreply.github.com>
2018-11-08 11:16:15 +0530

    Update sum.c

--- sum.c
+++ sum.c
@@ -1,16 +1,23 @@
-//program to find the sum inclusive of the start and the end no. mentioned
 /*
 Created on november 8, 2018
 @author: sumangala rao
 */
-#include<stdio.h>
+//program to find the sum inclusive of the start and the end no. mentioned
+  #include<stdio.h>
   int main()
-   {
-     int n1,n2,i,sum=0;         //initialization
-     scanf("%d %d",&n1,&n2);    //taking user input
-       //computing sum
-     for(i=n1;i<=n2;i++)
-        sum=sum+i; 
-     printf("%d",sum);           //display the sum
-    return 0;
-   }
+    {
+      //initialization
+      int n1,n2,i,sum=0;
+    
+      //taking user input
+      scanf("%d %d",&n1,&n2);
+    
+      //computing sum
+      for(i=n1;i<=n2;i++)
+          sum=sum+i;
+    
+      //display the sum
+     printf("%d",sum);  
+    
+  return 0;
+  }

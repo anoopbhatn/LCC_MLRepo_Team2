93ed0621ebb8f40a55dd2825cff77c101db11b6a
Prasada kp <44546156+prasadkpbhat@users.noreply.github.com>
2018-11-08 14:50:24 +0530

    Update sum.py

--- prasada/sum.py
+++ prasada/sum.py
@@ -3,6 +3,7 @@ Created on Thu Nov  8 12:34:32 2018
 @author: prasada
 input:two integers one is starting value and the ending value
 output:The sum of all the integers in the given range
+program to find the sum of all the integers in the given range(a,b),where a is starting integer and b is the ending integer.
 '''
 #initializing a variable to 0
 sum=0
@@ -12,6 +13,6 @@ start=int(input("start value"))
 end=int(input("end value"))
 #for loop to calculate the sum of all the integers in the given range
 for i in range(start,end+1,1):
-    sum+=i;
+    sum+=i
 #printing the result
 print(sum)
